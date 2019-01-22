8591825a2516f425f1112d640aaa9c4b3d6d9d3a
Joey Grolleman <joeygrolleman@gmail.com>
2019-01-22 11:28:43 +0100

    scoreboard comp created

---
+++ scoreboard/src/Scoreboard.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import Player from './Player'
+
+const players = [
+  {
+    id: 1,
+    name: 'Wouter',
+    score: 2
+  },
+  {
+    id: 2,
+    name: 'Mimi',
+    score: 5
+  },
+  {
+    id: 3,
+    name: 'Milan',
+    score: 4
+  }
+]
+
+export default class Scoreboard extends Component {
+  renderPlayer(player) {
+    return <Player
+        key={player.id}
+        name={player.name}
+        score={player.score}
+      
+    />
+  }
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <h1>Scoreboard</h1>
+        <ul>
+          {
+            players
+              .sort((a, b) => b.score - a.score)
+              .map(this.renderPlayer)
+          }
+        </ul>
+      </div>
+    )
+  }
+}
