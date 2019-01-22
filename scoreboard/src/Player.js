5739e2eb45ff45b15e3efa919a590e1c9a7732b2
Joey Grolleman <joeygrolleman@gmail.com>
2019-01-22 16:37:51 +0100

    added minusPoints function

--- scoreboard/src/Player.js
+++ scoreboard/src/Player.js
@@ -15,14 +15,21 @@ export default class Player extends Component {
   handleOnClick = () => {
     const {id, score} = this.props
     this.props.updatePlayerScore(id, score + 1)
+    console.log(`${id} ${score}`)
+  }
+
+  handleOnClickMinus = () => {
+    const {id, score} = this.props
+    this.props.updatePlayerScore(id, score - 1)
+    console.log(id+score)
   }
 
   render() {
     return (
       <li className="player">
         <p className="name">{ this.props.name }</p>
-        <MinusButton onClick={this.handleOnClick} />
         <p className="score">{ this.props.score }</p>
+        <MinusButton onClick={this.handleOnClickMinus} />
         <PlusButton onClick={this.handleOnClick} />
       </li> 
     )
